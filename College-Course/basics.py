7256a2beb5e30fc58cc77959ba22d6b48a35818e
Kanishk <kanishkkumarverma0@gmail.com>
2026-08-25 03:37:18 +0530

    College lec1 basics

---
+++ College-Course/basics.py
@@ -0,0 +1,12 @@
+""" age = 10
+print(age)
+print(type(age))
+age = "sixteen"
+print (age)
+print (type(age)) """
+""" a,b,c = 10,20,30
+print(a,b,c)
+a,b,c = 10,20
+print(a,b,c) """
+a=b=c=50
+print(a,b,c)
