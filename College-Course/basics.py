2922ad767970ab10969795f2f731cc96ea720a25
Kanishk <kanishkkumarverma0@gmail.com>
2026-08-25 22:50:55 +0530

    lec notes updated

--- College-Course/basics.py
+++ College-Course/basics.py
@@ -8,6 +8,49 @@ print (type(age)) """
 print(a,b,c)
 a,b,c = 10,20
 print(a,b,c) """
-a=b=c=50
+""" a=b=c=50
 print(a,b,c)
 print("Kanishk")
+x=10
+c=12+x
+print(c)
+a=10
+b=20
+c=-9
+d=-4
+e=((a if a<d else d) if a<c else (c if c<d else d)) if a<b else ((b if b<d else d) if b<c else (c if c<d else d))
+a=20
+b=12
+print(e)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+a=20
+b=12
+c=a+(34 and 4)
+print(c)
+print(15 and 8) """
+""" a=10
+print(a)
+print(-a) """
+""" text="Welcome to python programming"
+print("Welcome" in text)
+print("welcome" in text)
+print("kanishk" in text)
+print("Hari" not in text)
+a=25
+b=25
+print(a is b)
+print(id(a))
+print(id(b))
+name= input("Enter the name: ")
+print("You entered name as: ",name) """
+p=10
+r=20.66
+t=30.5
+simpleinterest=(p*r*t)/100
+print("Simple Interest is: ",simpleinterest)
