bd0185e36686044e538fd00cee7e48a2437d7e2f
Kanishk <kanishkkumarverma0@gmail.com>
2026-09-05 10:46:41 +0530

    Add notes

--- College-Course/basics2.py
+++ College-Course/basics2.py
@@ -1,13 +1,21 @@
-#python coding class 2
-x = eval(input("Enter the Number: "))
-print(x)
-str1 = "Python" 
-print(str1[3])
-print(str1[-3])
-
-for x in str1:
-    print(x)
-    for x in range(len(str1)-1,-1,-1):
-        print(str1[x])
-        for x in range(len(str1)):
-            print(str1[x])
+s1="aMIT kumar SINGH is a person. he lives in NY."
+s2=s1.upper()
+print(s1,s2)
+print(s1.lower())
+print(s1.swapcase())
+print(s1.title())
+print(s1.capitalize)
+print(s1.islower())
+print(s1.isupper())
+print(s1.isalnum())
+print(s1.isalpha())
+print(s1.isnumeric())
+print(s1.isprintable())
+print(s1.isdigit())
+print(s1.isidentifier())
+name="Roshan"
+place="Agra"
+age=21
+#s1="{n} is {a} years old and lives in {p}".format(p=place, a=age, n=name)
+s1= f"{name} is {age} years old and lives in {place}"
+print(s1)
