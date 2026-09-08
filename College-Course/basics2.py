6eff848f7cb22690598d7a607c91579ac34c7869
Kanishk <kanishkkumarverma0@gmail.com>
2026-09-08 14:41:56 +0530

    Add notes

--- College-Course/basics2.py
+++ College-Course/basics2.py
@@ -1,21 +1,24 @@
-s1="aMIT kumar SINGH is a person. he lives in NY."
-s2=s1.upper()
-print(s1,s2)
-print(s1.lower())
-print(s1.swapcase())
-print(s1.title())
-print(s1.capitalize)
-print(s1.islower())
-print(s1.isupper())
-print(s1.isalnum())
-print(s1.isalpha())
-print(s1.isnumeric())
-print(s1.isprintable())
-print(s1.isdigit())
-print(s1.isidentifier())
-name="Roshan"
-place="Agra"
-age=21
-#s1="{n} is {a} years old and lives in {p}".format(p=place, a=age, n=name)
-s1= f"{name} is {age} years old and lives in {place}"
-print(s1)
+x=int(input("Enter a number: "))
+if x<0:
+    print("Negative number")
+else:
+    print("The number is positive")
+
+user_name ='rahul'
+x=input("Enter your name: ")
+if user_name == x.lower() :
+    print("The name is valid")
+else:
+    print("The name is invalid")
+
+x=1
+while x<=5:
+    print(x)
+    x+=1
+    print("End")
+
+x=10
+while (x>=10) and (x<=20):
+    print(x)
+    x+=2
+    print("End")
